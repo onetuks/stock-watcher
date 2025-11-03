fb7eab6c9c7b985bfd93fb1fb4e858a489b3e478
onetuks <onetuks@naver.com>
2025-11-04 00:30:46 +0900

    Data: 관심종목 데이터 변경

--- data/watchlist.py
+++ data/watchlist.py
@@ -6,4 +6,9 @@ import streamlit
 def load_watchlist(path="data/watchlist.csv"):
   return pandas.read_csv(path)
 
+
 watchlist = load_watchlist()
+
+
+def get_ticker(idx: int) -> str:
+  return watchlist["ticker"][idx]
